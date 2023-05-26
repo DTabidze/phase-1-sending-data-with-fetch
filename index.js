da4ed0ef96146cfce2c7c2a1b10d311df48cd11d
Davit Tabidze <davidtabidzef1@gmail.com>
2023-05-26 13:59:57 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Add your code here
+function submitData (userName,email) {
+    let userNameObj = {};
+    userNameObj["name"] = userName;
+    userNameObj["email"] = email;
+    const URL = "http://localhost:3000/users";
+    return fetch (URL, {
+        method:"POST",
+        headers:{
+            "Content-Type":"application/json",
+            "Accept":"application/json"
+        },
+        body:JSON.stringify(userNameObj)
+    })
+    .then(response => response.json())
+    .then(data => {
+        const newId = data.id;
+        document.body.innerHTML += newId;
+      })
+    .catch(error => document.body.innerHTML += error.message);
+}
+
+
